188b3b30a74aa0f537e91eb65bbd45560bf3f8a4
Mantas Latkauskas <mantas.latkauskas@atlasvpn.com>
2024-03-11 16:16:23 +0200

    Add iOS implementation for maps

---
+++ composeApp/src/iosMain/kotlin/FullMap.kt
@@ -0,0 +1,73 @@
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.interop.UIKitView
+import cocoapods.GoogleMaps.GMSCameraPosition
+import cocoapods.GoogleMaps.GMSMapView
+import cocoapods.GoogleMaps.GMSMapViewDelegateProtocol
+import kotlinx.cinterop.BetaInteropApi
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.ExportObjCClass
+import kotlinx.cinterop.useContents
+import platform.CoreLocation.CLLocationCoordinate2DMake
+import platform.darwin.NSObject
+import ui.util.CameraPosition
+import ui.util.Location
+
+@OptIn(ExperimentalForeignApi::class)
+@Composable
+actual fun FullMap(
+    modifier: Modifier,
+    initialCameraPosition: CameraPosition,
+    onSelectionChange: (Location) -> Unit,
+) {
+    val mapViewDelegate = remember {
+        MapViewDelegate(onCameraIdle = onSelectionChange)
+    }
+
+    val mapView = remember {
+        GMSMapView().apply {
+            delegate = mapViewDelegate
+            camera = GMSCameraPosition.cameraWithTarget(
+                CLLocationCoordinate2DMake(initialCameraPosition.target.latitude, initialCameraPosition.target.longitude),
+                initialCameraPosition.zoom.toFloat()
+            )
+            setMyLocationEnabled(true)
+        }
+    }
+
+    UIKitView(
+        modifier = modifier,
+        factory = { mapView },
+        update = { view ->
+            initialCameraPosition.let {
+                view.setCamera(
+                    GMSCameraPosition.cameraWithLatitude(
+                        it.target.latitude,
+                        it.target.longitude,
+                        it.zoom
+                    )
+                )
+            }
+        }
+    )
+}
+
+
+@OptIn(BetaInteropApi::class, ExperimentalForeignApi::class)
+@ExportObjCClass
+class MapViewDelegate(private val onCameraIdle: (Location) -> Unit) : NSObject(), GMSMapViewDelegateProtocol {
+    @OptIn(ExperimentalForeignApi::class)
+    override fun mapView(
+        mapView: GMSMapView,
+        @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
+        idleAtCameraPosition: GMSCameraPosition
+    ) {
+        idleAtCameraPosition.target.useContents {
+            onCameraIdle(Location(this.latitude, this.longitude))
+        }
+    }
+}
+
+
---
+++ composeApp/src/iosMain/kotlin/LockedMap.kt
@@ -0,0 +1,40 @@
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.interop.UIKitView
+import cocoapods.GoogleMaps.GMSCameraPosition
+import cocoapods.GoogleMaps.GMSMapView
+import kotlinx.cinterop.ExperimentalForeignApi
+import ui.util.CameraPosition
+
+@OptIn(ExperimentalForeignApi::class)
+@Composable
+actual fun LockedMap(
+    modifier: Modifier,
+    cameraPosition: CameraPosition,
+) {
+    val mapView = remember {
+        GMSMapView().apply {
+            setMyLocationEnabled(true)
+            settings.setAllGesturesEnabled(false)
+            settings.myLocationButton = false
+        }
+    }
+
+    UIKitView(
+        modifier = modifier,
+        factory = { mapView },
+        update = {view ->
+            cameraPosition.let {
+                view.setCamera(
+                    GMSCameraPosition.cameraWithLatitude(
+                        it.target.latitude,
+                        it.target.longitude,
+                        it.zoom
+                    )
+                )
+            }
+        }
+    )
+}
